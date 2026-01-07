4b60e3aa90df9ecd75ed8a31667479c6f9555b6c
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-06 18:53:39 -0600

    Add CategoryDto and CreateCategoryDto for category data transfer

---
+++ Models/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiEcommerce.Models.Dtos;
+
+public class CreateCategoryDto
+{
+    [Required(ErrorMessage = "The Name field is required.")]
+    [MaxLength(50, ErrorMessage = "The Name field must not exceed 50 characters.")]
+    [MinLength(3, ErrorMessage = "The Name field must be at least 3 characters.")]
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreationDate { get; set; }
+}
