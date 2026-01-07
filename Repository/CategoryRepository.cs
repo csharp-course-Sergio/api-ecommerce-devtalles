e118b0010d5bb73cbb4bf2cff79d2b95023e62e8
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-06 18:45:53 -0600

    Add CategoryRepository implementation for category management

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,54 @@
+using ApiEcommerce.Repository.IRepository;
+
+namespace ApiEcommerce.Repository;
+
+public class CategoryRepository(ApplicationDbContext db) : ICategoryRepository
+{
+    private readonly ApplicationDbContext _db = db;
+
+    public bool CategoryExists(int id)
+    {
+        return _db.Categories.Any(category => category.Id == id);
+    }
+
+    public bool CategoryExists(string name)
+    {
+        return _db.Categories.Any(category => category.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+
+    public bool CreateCategory(Category category)
+    {
+        category.CreationDate = DateTime.Now;
+
+        _db.Categories.Add(category);
+        return Save();
+    }
+
+    public bool DeleteCategory(Category category)
+    {
+        _db.Categories.Remove(category);
+        return Save();
+    }
+
+    public ICollection<Category> GetCategories()
+    {
+        return [.. _db.Categories.OrderBy(category => category.Name)];
+    }
+
+    public Category GetCategory(int id)
+    {
+        return _db.Categories.FirstOrDefault(category => category.Id == id) ?? throw new InvalidOperationException($"Category with Id: {id} was not found.");
+    }
+
+    public bool Save()
+    {
+        return _db.SaveChanges() >= 0;
+    }
+
+    public bool UpdateCategory(Category category)
+    {
+        category.CreationDate = DateTime.Now;
+        _db.Categories.Update(category);
+        return Save();
+    }
+}
