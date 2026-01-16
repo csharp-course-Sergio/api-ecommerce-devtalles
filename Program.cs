b2d4e9b16aba79fca72fd4edb2525a579304b1be
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-16 11:45:49 -0600

    Disable audience validation in JWT authentication configuration

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
         ValidateIssuer = false,
-        ValidateAudience = true
+        ValidateAudience = false
     };
 });
 
