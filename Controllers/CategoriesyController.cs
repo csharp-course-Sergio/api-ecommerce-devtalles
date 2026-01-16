34bfc6eebb21d1ddd5dcb281cd832a379064e5fa
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-16 11:55:49 -0600

    Update authorization roles to lowercase for consistency and enhance Swagger documentation for JWT authentication

--- Controllers/CategoriesyController.cs
+++ Controllers/CategoriesyController.cs
@@ -8,7 +8,7 @@ namespace ApiEcommerce.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "admin")]
     // [EnableCors(PolicyNames.AllowSpecificOrigin)]
     public class CategoriesController(ICategoryRepository categoryRepository, IMapper mapper) : ControllerBase
     {
